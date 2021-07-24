a66381f24064d4c0a02d54f50d5642d48d8bf5fa
Asif Estiak <me.asif.ae@gmail.com>
2021-07-24 22:44:41 +0600

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ import './App.css';
 function App() {
   return (
     <Fragment>
+
+      {/* Header */}
+      <header id="header">
+        <nav className="navbar navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Navbar</a>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+      </header>
+      
     </Fragment>
   );
 }
