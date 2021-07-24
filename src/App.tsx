61fb8c1a938ea409b302646853b618cc2a9194d5
Asif Estiak <me.asif.ae@gmail.com>
2021-07-24 23:11:06 +0600

    changed some styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
 
       {/* Header */}
       <header id="header">
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-navy">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">Navbar</a>
+            <a className="navbar-brand" href="/">Wire Note</a>
             <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <button className="btn btn-outline-light" type="submit">Search</button>
             </form>
           </div>
         </nav>
